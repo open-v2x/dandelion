4db595798b1b9a995208953becc4d9b8cacd8ed8
dlsuqin <110013522+dlsuqin@users.noreply.github.com>
2022-09-08 17:55:46 +0800

    fix: newly added RSU show center (#78) (#96)
    closes #77
    cherry pick: #78
    
    Co-authored-by: ranmc <ranmc@foxmail.com>

--- dandelion/mqtt/server.py
+++ dandelion/mqtt/server.py
@@ -31,6 +31,9 @@ from dandelion.mqtt.service.cloud.edge_delete import EdgeDeleteRouterHandler
 from dandelion.mqtt.service.cloud.edge_hb import EdgeHBRouterHandler
 from dandelion.mqtt.service.cloud.edge_info import EdgeInfoRouterHandler
 from dandelion.mqtt.service.cloud.edge_rsu import EdgeRSURouterHandler
+from dandelion.mqtt.service.cloud.edge_rsu_add import EdgeRSUAddRouterHandler
+from dandelion.mqtt.service.cloud.edge_rsu_delete import EdgeRSUDeleteRouterHandler
+from dandelion.mqtt.service.cloud.edge_rsu_location import EdgeRSULocationRouterHandler
 from dandelion.mqtt.service.map.map_down import MapDownACKRouterHandler
 from dandelion.mqtt.service.map.map_up import MapRouterHandler
 from dandelion.mqtt.service.query.rsu_query_up import RSUQueryUPRouterHandler
@@ -65,6 +68,9 @@ if mode_conf.mode in ["center", "coexist"]:
     topic_router[v2x_edge.V2X_EDGE_INFO_UP] = EdgeInfoRouterHandler()
     topic_router[v2x_edge.V2X_EDGE_HB_UP] = EdgeHBRouterHandler()
     topic_router[v2x_edge.V2X_EDGE_RSU_UP] = EdgeRSURouterHandler()
+    topic_router[v2x_edge.V2X_EDGE_RSU_ADD_UP] = EdgeRSUAddRouterHandler()
+    topic_router[v2x_edge.V2X_EDGE_RSU_DELETE_UP] = EdgeRSUDeleteRouterHandler()
+    topic_router[v2x_edge.V2X_EDGE_RSU_LOCATION_UP] = EdgeRSULocationRouterHandler()
     topic_router[v2x_edge.V2X_EDGE_DELETE_UP] = EdgeDeleteRouterHandler()
 
 
