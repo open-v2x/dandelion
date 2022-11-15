4980d6161dea7fbde95838628b5181567eba4c0f
qiaolei <57551157+qiaolei1@users.noreply.github.com>
2022-11-15 15:14:57 +0800

    fix: get user from config file (#142)
    Co-authored-by: 99cloud <99cloud@99clouddeMacBook-Pro.local>

--- dandelion/conf/user.py
+++ dandelion/conf/user.py
@@ -16,15 +16,36 @@ from __future__ import annotations
 
 from oslo_config import cfg
 
-from dandelion.conf import cors, database, mode, mqtt, redis, token, user
+user_group = cfg.OptGroup(
+    name="user",
+    title="user Options",
+    help="""
+user related options.
+""",
+)
 
-CONF: cfg = cfg.CONF
+user_opts = [
+    cfg.StrOpt(
+        "username",
+        default="admin",
+        help="""
+username.
+""",
+    ),
+    cfg.StrOpt(
+        "password",
+        default="dandelion",
+        help="""
+user password.
+""",
+    ),
+]
 
 
-cors.register_opts(CONF)
-database.register_opts(CONF)
-mqtt.register_opts(CONF)
-redis.register_opts(CONF)
-token.register_opts(CONF)
-mode.register_opts(CONF)
-user.register_opts(CONF)
+def register_opts(conf):
+    conf.register_group(user_group)
+    conf.register_opts(user_opts, group=user_group)
+
+
+def list_opts():
+    return {user_group: user_opts}
